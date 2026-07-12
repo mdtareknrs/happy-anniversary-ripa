42e2b43593a78995107e50cf9963e7b5e7354380
Md Tarekul Islam <mdtarekulislam3@gmail.com>
2026-07-13 00:16:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.getElementById("startBtn").addEventListener("click", function () {
+    document.body.innerHTML = `
+    <div style="height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    background:#ff4b6e;
+    color:white;
+    text-align:center;
+    padding:20px;">
 
+    <h1>❤️ প্রিয় রিপা ❤️</h1>
+
+    <h3>শুভ ৪র্থ বিবাহবার্ষিকী</h3>
+
+    <p>
+    তোমাকে আমার জীবনে পাওয়াটা
+    আল্লাহর সবচেয়ে সুন্দর উপহার।
+    <br><br>
+    ভালোবাসি...
+    আজও...
+    আগামীতেও...
+    সবসময়।
+    </p>
+
+    </div>`;
+});
